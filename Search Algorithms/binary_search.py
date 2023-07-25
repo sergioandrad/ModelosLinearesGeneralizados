6205b99c909f967fb4977749521541bbcc733950
sergioandrad <39348598+sergioandrad@users.noreply.github.com>
2023-07-25 20:06:40 -0300

    Update binary_search.py

--- Search Algorithms/binary_search.py
+++ Search Algorithms/binary_search.py
@@ -14,20 +14,21 @@ x   = vec[5]
 
 # Function that bisects the vector until we find x, otherwise
 # it returns -1 to signal that x do not belong to the vector
-def rank(x, vec):
-    lo_b = 0
-    hi_b = len(vec)-1
-    while lo_b <= hi_b:
-        mid = int(lo_b + (hi_b - lo_b)/2)
-        if x < vec[mid]: 
-            hi_b = mid-1
-        elif x > vec[mid]:
-            lo_b=mid+1
+def search(self, nums: List[int], target: int) -> int:
+    import math
+    L = 0
+    R = len(nums) - 1
+    while (L <= R):
+        m = (L + R) // 2
+        if nums[m] < target:
+            L = m + 1
+        elif nums[m] > target:
+            R = m - 1
         else:
-            return mid
-    return -1
+            return (m)
+    return (-1)
 
 # Testing the binary search function
 x = 3
-resp = rank(x=x,vec=vec)
+resp = search(x=x,vec=vec)
 x == vec[resp]
